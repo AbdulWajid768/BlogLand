2c20f24ecc724953e772a70f315f5571632f0218
AbdulWajid768 <62804593+AbdulWajid768@users.noreply.github.com>
2021-02-04 00:13:18 +0500

    Adding Project Files

---
+++ wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+function showLoginPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showSignupPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showSignupConPassword() {
+    var x = document.getElementById("conPassword");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
